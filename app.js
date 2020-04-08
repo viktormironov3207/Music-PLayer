f35c9e4bd3c67b51701a09ce34a786823cc8ff71
Viktor Mironov <viktormironov3207@abv.bg>
2020-04-08 18:08:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+//Song titles
+const songs = ['Alex Alta - Mari Mariiko','Luar - Gjithmone','Hot Natured & Ali Love - Benediction','DEEP ZONE Project - Say My Name','Volkan Uça feat. Merih - İstanbul','Natema - Freaks in d pressure'];
+
+//Keep track of song
+let songIndex = 2;
+
+//Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`
+}
+//Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+//Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+//Previous song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+//Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+//Update Progress
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`
+}
+//Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+//Event listners 
+playBtn.addEventListener('click', () => {
+  const isPLaying = musicContainer.classList.contains('play');
+
+  if (isPLaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+//Change Song 
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//Time/Song update 
+audio.addEventListener('timeupdate', updateProgress);
+
+//Progress bar click
+progressContainer.addEventListener('click', setProgress);
+
+//Song ends
+audio.addEventListener('ended', nextSong);
